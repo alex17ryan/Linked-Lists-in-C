4f96785ddabf519e607d8068837dd22a2966276a
Alex Ryan <115376787+alex17ryan@users.noreply.github.com>
2023-05-07 09:28:29 +0100

    Update create_list.c

--- create_list.c
+++ create_list.c
@@ -6,56 +6,19 @@ struct node {
     struct node *next;
 };
 
-struct node *create_list(struct node *);
-struct node *create_node(int);
-struct node *add_node_at_end(struct node *, int);
-void display_list(struct node *);
-
-int main() {
-
-    struct node *head = NULL;
-
-    head = create_list(head);
-    display_list(head);
-
-    return 0;
-
-}
-
-struct node *create_list(struct node *h) {
-
-    int d;
-
-    while (1) {
-
-        printf("Enter data: "); scanf("%i", &d);
-
-        if (d == -1)
-            break;
-        else
-            h = add_node_at_end(h, d);
-
-    }
-
-    return h;
-
-}
-
-struct node *add_node_at_end(struct node *h, int d) {
+void display_list(struct node *h) {
 
     struct node *temp = h;
-    
+
     if (h == NULL) {
-        h = create_node(d);
+        printf("The list is empty.");
     }
     else {
-        while (temp->next != NULL) {
+        while (temp != NULL) {
+            printf("%i ", temp->data);
             temp = temp->next;
         }
-        temp->next = create_node(d);
     }
-
-    return h;
 
 }
 
@@ -66,13 +29,48 @@ struct node *create_node(int d) {
     return new;
 }
 
-void display_list(struct node *h) {
-    
-    struct node *temp = h;
+void add_node_at_last(struct node **h, int d) {
 
-    while(temp != NULL) {
-        printf("%i ", temp->data);
-        temp = temp->next;
+    struct node *temp = *h;
+
+    if (*h == NULL) {
+        *h = create_node(d);
+    }
+    else {
+        while (temp->next != NULL) {
+            temp = temp->next;
+        }
+        temp->next = create_node(d);
     }
 
+}
+
+void create_list(struct node **h) {
+
+    int d;
+
+    while (1) {
+
+        printf("Enter data: "); scanf("%i", &d);
+
+        if (d == -1) {
+            break;
+        }
+        else {
+            add_node_at_last(&*h, d);
+        }
+
+    }
+
+}
+
+int main() {
+
+    struct node *head = NULL;
+
+    create_list(&head);
+    display_list(head);
+
+    return 0;
+    
 }
